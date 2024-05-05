87aee30ecd54d8cf903f8eccc66809bf16e16a3d
prialbino <pbatistela@hotmail.com>
2024-05-05 18:12:12 -0500

    Create Booking ID and Get Booking Methods with Robot Framework

--- resources/variables.py
+++ resources/variables.py
@@ -1,3 +1,10 @@
 #Variables - valores fixos
 url = 'https://restful-booker.herokuapp.com'
 content_type = 'application/json'     # data format to be transmitted
+firstname = 'Alan'
+lastname = 'Williams'
+totalprice = 150
+depositpaid = False
+bookingdates = dict(checkin = '2024-04-27', checkout = '2024-04-27')
+additionalneeds = 'Breakfast'
+
